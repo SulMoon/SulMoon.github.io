d0b95e2a91bfbf1064614db2afae8bb1c628e52b
Lzf Cloud <cloudlzf@163.com>
2026-02-28 21:14:29 +0800

    修改读取方式

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,21 +1,21 @@
-var filepath = "/datas/md/file.md";
-var fullPath = location.origin + filepath;
-var rawFile = new XMLHttpRequest();
-rawFile.open("GET", fullPath, false);
-rawFile.onreadystatechange = function () {
-    if (rawFile.readyState === 4) {
-        if (rawFile.status === 200 || rawFile.status == 0) {
-            var allText = rawFile.responseText;
-            console.log(allText);
-            document.getElementById("markdown").innerHTML = marked(allText);
-            // document.getElementById("markdown").innerHTML = marked.parse(rawtext);
-        }
-    }
-}
+// var filepath = "/datas/md/file.md";
+// var fullPath = location.origin + filepath;
+// var rawFile = new XMLHttpRequest();
+// rawFile.open("GET", fullPath, false);
+// rawFile.onreadystatechange = function () {
+//     if (rawFile.readyState === 4) {
+//         if (rawFile.status === 200 || rawFile.status == 0) {
+//             var allText = rawFile.responseText;
+//             console.log(allText);
+//             document.getElementById("markdown").innerHTML = marked(allText);
+//             // document.getElementById("markdown").innerHTML = marked.parse(rawtext);
+//         }
+//     }
+// }
 
-// fetch('../datas/md/file.md')
-// .then(res => res.text())
-// .then(md => { 
-//     const html = marked.parse(md); 
-//     document.getElementById('markdown').innerHTML = html; 
-// });
+fetch('../datas/md/file.md')
+.then(res => res.text())
+.then(md => { 
+    const html = marked.parse(md); 
+    document.getElementById('markdown').innerHTML = html; 
+});
